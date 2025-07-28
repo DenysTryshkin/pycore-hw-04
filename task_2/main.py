ff1fdb6ddf74eca1f1fb1f2fb3cae66924ca0f2b
Denys Tryshkin <denystryshkin@gmail.com>
2025-07-28 13:16:53 -0400

    added task_2

---
+++ task_2/main.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+def get_cats_info(path):
+    cats_info = []
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            for line in f:
+                id_, name, age = line.strip().split(",")
+                cats_info.append({
+                    "id": id_,
+                    "name": name,
+                    "age": age})
+        return cats_info
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+
+cats_info = get_cats_info("/Users/denys/Desktop/python/pycore-hw-04/cats_file.txt")
+print(cats_info)
